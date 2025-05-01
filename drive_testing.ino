05fe89a22248c00ed90ec26223b69f2c20b3494a
nicholaskiefer <Nicholas.kiefer2@gmail.com>
2025-05-01 10:59:32 +0200

    complete rewrite

--- drive_testing.ino
+++ drive_testing.ino
@@ -1,4 +1,3 @@
-
 #if !defined(ARDUINO_ESP32_DEV) && !defined(ARDUINO_INKPLATE6V2)
 #error "Wrong board selection for this example, please select e-radionica Inkplate6 or Soldered Inkplate6 in the boards menu."
 #endif
@@ -6,29 +5,137 @@
 #include "Inkplate.h"    //Include Inkplate library to the sketch
 #include <HTTPClient.h>  //Include HTTP library to this sketch
 #include <WiFi.h>        //Include ESP32 WiFi library to our sketch
-#include "base64.hpp"
 #include "wifistuff.h"
-#include "DSTManager.h"
+
+//#define WAKE_BUTTON_PIN 39 // double-check actual pin from schematic or documentation
 
 unsigned long ms = millis();
 unsigned long readMillis = 0;
 String displaytext;
 
-Inkplate display(INKPLATE_1BIT);  // Create an object on Inkplate library and also set library into 1 Bit mode (BW)
-int statusBarHeight = 12;
+//Inkplate display(INKPLATE_1BIT);  // Create an object on Inkplate library and also set library into 1 Bit mode (BW)
+Inkplate display(INKPLATE_3BIT);
+bool staticip = false;
 
 struct Result {
-  int size;
-  int pos_x;
-  int pos_y;
-  String text;
+  String firstLine;
+  String remainingText;
 };
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000);  // NTP client
+int cursorline = 0;
+int updateMs = 30000;
 
 
 void setup() {
+  delay(1000);
+  Serial.begin(115200);
+
+  setup_display();
+  setup_wifi();
+  delay(1000);
+  analogReadResolution(12);
+  //pinMode(GPIO_NUM_36, INPUT_PULLUP); // Set wake-up button as input
+}
+
+void loop() {
+  // check time
+  if (!(millis() - readMillis > updateMs || readMillis == 0 || detect_button())) {
+    return;
+  }
+
+  // check internet connection
+  readMillis = millis();
+  if (WiFi.status() != WL_CONNECTED) {
+    // keep counter, wait 1 min and try again
+    Serial.println("WiFi disconnected.");
+    return;
+  }
+  // check delta
+  // update
+  HTTPClient http;
+  http.getStream().setNoDelay(true);
+  http.getStream().setTimeout(1);
+  http.begin(download);
+  int responsecode = http.GET();
+  int bodyLen = http.getSize();
+  String encodedString = http.getString();
+  String responsecodestr = String(responsecode);
+  if (responsecode != 200) {
+    String message = "http response code: " + String(responsecode);
+    Serial.println(message);
+    return;
+    }
+  if (encodedString == displaytext) {
+    Serial.println("encoded string is already displayed.");
+    return;
+    }
+
+
+  displaytext = encodedString;
+  Result res = parseInput(encodedString);
+
+
+  if (res.firstLine == String("text")) {
+    display_text(res);
+    }
+
+  if (res.firstLine == String("web")) {
+    display_git(res);
+    }
+  return;
+}
+
+Result parseInput(String input) {
+  int newlineIndex = input.indexOf('\n');
+  String firstLine = input.substring(0, newlineIndex);
+  String remainingText = input.substring(newlineIndex + 1);
+  return {firstLine, remainingText};
+}
+
+void display_text(Result result) {
+  String remainingText = result.remainingText;
+  int secondLineIndex = remainingText.indexOf("\n");
+  String secondLine = remainingText.substring(0, secondLineIndex);
+  String textToDisplay = remainingText.substring(secondLineIndex + 1);
+  
+  // second row is like: >3 25,25<    first int is text size, rest is position
+  int firstSpaceIndex = secondLine.indexOf(' ');
+  int size = secondLine.substring(0, firstSpaceIndex).toInt();
+
+  // Parse the position values (pos_x and pos_y)
+  String posPart = secondLine.substring(firstSpaceIndex + 1);
+  int commaIndex = posPart.indexOf(',');
+
+  int pos_x = posPart.substring(0, commaIndex).toInt();
+  int pos_y = posPart.substring(commaIndex + 1).toInt();
+    // print text to display given size and posx posy
+    display.clearDisplay();
+    display.setTextSize(size);
+    display.setCursor(pos_x, pos_y);
+    display.setTextColor(BLACK);
+    display.print(textToDisplay);
+    display.display();
+    display.clearDisplay();
+  return;
+}
+
+void display_git(Result result) {
+    // display github bmp
+    int secondLineIndex = result.remainingText.indexOf("\n");
+    String downloadlink = result.remainingText.substring(0, secondLineIndex);
+    String message = "download link: " + downloadlink;
+    Serial.println(message);
+    display.clearDisplay();
+    if (!display.drawImage(downloadlink, 0, 0, false, false)) {
+      Serial.println("image error. Wrong url or wrong image encoding.");
+      return;
+    }
+    display.display();
+    Serial.println("showing image from download.");
+    return;
+}
+
+void setup_display() {
   display.begin();                     // Init Inkplate library (you should call this function ONLY ONCE)
   display.clearDisplay();              // Clear frame buffer of display
   display.setTextSize(2);              // Set text scaling to two (text will be two times bigger)
@@ -36,93 +143,84 @@ void setup() {
 
   display.clearDisplay();           // Clear everything in frame buffer
   display.setCursor(0, 0);          // Set print cursor to new position
-  display.print("Connecting to ");  // Print the name of WiFi network
-  display.print(ssid);
-  WiFi.begin(ssid, pass);  // Try to connect to WiFi network
+  display.display();
+}
+
+void setup_wifi() {
+  //scan_wifi();  // first a scan
+
+  WiFi.mode(WIFI_STA);
   WiFi.setAutoReconnect(true);
+  if (staticip) {
+    IPAddress local_IP(192,168,1,184);
+    IPAddress gateway(192,168,1,1);
+    IPAddress subnet(255,255,255,0);
+    WiFi.config(local_IP, gateway, subnet);
+  }
+  
+  String message = "Connecting to " + String(ssid) + "...";
+  print(message);
+  WiFi.begin(ssid, pass);  // Try to connect to WiFi network
+  Serial.println("SSID: [" + String(ssid) + "]");
+  Serial.println("PASS: [" + String(pass) + "]");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);  // While it is connecting to network, display dot every second, just to know that Inkplate is
-                  // alive.
-    display.print('.');
-    display.partialUpdate();
+    delay(1000);
+    String message = "status: " + String(WiFi.status());
+    Serial.println(message);
   }
-  display.print("connected");  // If it's connected, notify user
-  display.partialUpdate();
+  print("Connected.");
+  Serial.println("Connected.");
   display.clearDisplay();
-  display.setCursor(0, 0);
-  delay(1);
   display.display();
-  //testsite();
-  //statusbar();
-  timeClient.begin();
-}
-
-void loop() {
-  if (!(millis() - readMillis > 10000 || readMillis == 0)) {
-    return;
-  }
-  readMillis = millis();
-  // If wifi is down, do nothing else and print status off
-  if (WiFi.status() != WL_CONNECTED) {
-    // should reconnect to network? dont care todo
-    // statusbar("off", "off");
-    return;
-  }
-  HTTPClient http;
-  http.begin(download);
-  int responsecode = http.GET();
-  int bodyLen = http.getSize();
-  String encodedString = http.getString();
-  String responsecodestr = String(responsecode);
-  if (responsecode != 200) {
-    //statusbar("on", responsecodestr);
-    return;
-  }
-  if (encodedString == displaytext) {
-    return;
-  }
-  displaytext = encodedString;
-  Result res = parseInput(encodedString);
-  // print text to display given size and posx posy
-  display.clearDisplay();
-  display.setTextSize(res.size);
-  display.setCursor(res.pos_x, res.pos_y);
-  display.print(res.text);
-  display.display();
-  display.clearDisplay();
-}
-
-Result parseInput(String input) {
-  int newlineIndex = input.indexOf('\n');
-  String firstLine = input.substring(0, newlineIndex);
-  String remainingText = input.substring(newlineIndex + 1);
-
-  // Parse the first int (size)
-  int firstSpaceIndex = firstLine.indexOf(' ');
-  int size = firstLine.substring(0, firstSpaceIndex).toInt();
-
-  // Parse the position values (pos_x and pos_y)
-  String posPart = firstLine.substring(firstSpaceIndex + 1);
-  int commaIndex = posPart.indexOf(',');
-
-  int pos_x = posPart.substring(0, commaIndex).toInt();
-  int pos_y = posPart.substring(commaIndex + 1).toInt();
-
-  return { size, pos_x, pos_y, remainingText };
-}
-
-void statusbar(String WifiStatus, String SiteStatus) {
-  timeClient.update();
-  updateDST(timeClient);  // Call the DST adjustment function
-  String formattedTime = timeClient.getFormattedTime();
-  String Status = formattedTime + "";
-  for (int i = 0; i < 30; i++) {
-    Status += " ";
-  }
-  Status += "WiFi: " + WifiStatus + " Site: " + SiteStatus;
   display.setCursor(0, 0);
-  display.setTextSize(11);
-  display.print(Status);
+  cursorline = 0;
+}
+
+void print(const String& message) {
+  int length = message.length();
+  int chunklength = 29;
+  for (int i = 0; i<length; i += chunklength) {
+    if (cursorline == 28) {
+      //display.clearDisplay();
+      display.setCursor(0, 0);
+      cursorline = 0;
+    }
+    String chunk = message.substring(i, i + chunklength);
+    display.print("                                  ");
+    display.print(chunk);
+    display.print("\n");
+    cursorline++;
+  }
   display.partialUpdate();
-  display.clearDisplay();
+}
+
+void scan_wifi() {
+  print("Scanning WiFis...");
+  int n = WiFi.scanNetworks();
+  print("Scan complete.");
+  if (n == 0) {
+    print("No networks found.");
+  } else {
+    String message = "found " + String(n) + " networks";
+    print(message);
+    for (int i = 0; i < n; ++i) {
+      message = String(i + 1) + ": " + WiFi.SSID(i) + " (RSSI: " + String(WiFi.RSSI(i)) + " dBm)";
+      print(message);
+    }
+  }
+}
+
+bool detect_button() {
+  int rawValue = analogRead(36);
+  float voltage = (rawValue / 4095.0) * 3.3;
+  // Threshold (tune based on your hardware — usually < 0.5V when pressed)
+  bool buttonPressed = voltage < 0.5;
+  if (buttonPressed) {
+    Serial.println("Wake button pressed! updating...");
+    // trigger refresh instead of waiting
+    // debounce button
+    delay(500);
+    return true;
+  }
+  return false;
 }
