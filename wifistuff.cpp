8694a086ed7444a77adfd1c8b70cb04f988bca9d
Nicholas Kiefer <nicholas.kiefer@kit.edu>
2026-03-26 18:41:42 +0100

    delete

--- wifistuff.cpp
+++
@@ -1,6 +0,0 @@
-#include "Inkplate.h"   //Include Inkplate library to the sketch
-#include "wifistuff.h"
-
-String ssid = "fermiwlan";
-String pass = "z57wohnung";
-String download = "https://raw.githubusercontent.com/NicholasKiefer/inkplate_showcase/refs/heads/OTA-updates/manifest.txt";
